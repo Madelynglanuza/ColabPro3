d429b878c4e894eae15a20c4d126a0294a00ebe8
Danild Zambrana <42362162+DanildZambrana@users.noreply.github.com>
2024-10-17 00:29:12 -0600

    feat: Added NavBar Component

---
+++ views/components/NavBar.js
@@ -0,0 +1,23 @@
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({mode: 'open'});
+
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', 'components/NavBar.css')
+
+        shadow.appendChild(linkElem);
+
+        fetch('components/NavBar.html')
+            .then(response => response.text())
+            .then(html => {
+                const container = document.createElement('nav');
+                container.classList.add('navbar');
+                container.innerHTML = html;
+                shadow.appendChild(container);
+            })
+    }
+}
+
+customElements.define('colabpro-navbar', NavBar);
