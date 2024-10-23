a6cc536f479f2b14bf93b778b0c3877745189472
Danild Zambrana <42362162+DanildZambrana@users.noreply.github.com>
2024-10-22 22:22:30 -0600

    Se ha optimizado el codigo del local storage y se corrigio el metodo registerSavable para que admita una lista de clases como parametros

--- scripts/modules/localStorage.js
+++ scripts/modules/localStorage.js
@@ -1,15 +1,17 @@
-import {Savable} from "./Savable.js";
-
-const savable_classes = [];
+import { Savable } from "./Savable.js";
 
 export class LocalStorage {
     static savable_classes = {};
 
-    static registerSavableClass(savableClass) {
-        if (!(savableClass.prototype instanceof Savable)) {
-            throw new Error('Class must inherit from Savable');
+    static registerSavableClass(...savableClasses) {
+        for (const savableClass of savableClasses) {
+            if (!(savableClass.prototype instanceof Savable)) {
+                throw new Error('Class must inherit from Savable');
+            }
+            LocalStorage.savable_classes[savableClass.name] = savableClass;
         }
-        LocalStorage.savable_classes[savableClass.name] = savableClass;
+
+        console.log(LocalStorage.savable_classes);
     }
 
     constructor() {
@@ -36,18 +38,25 @@ export class LocalStorage {
     getAllItems(itemClass) {
         const key = `${itemClass.name.toLowerCase()}_storage`;
         const items = JSON.parse(this.storage.getItem(key)) || {};
-        // Deserialize the items
         const deserializedItems = {};
+
         for (const id in items) {
-            let item = itemClass.fromJSON(items[id]);
-            this.replaceReferences(item);
-            deserializedItems[id] = item;
+            if (items.hasOwnProperty(id)) {
+                try {
+                    let item = itemClass.fromJSON(items[id]);
+                    this.replaceReferences(item);
+                    deserializedItems[id] = item;
+                } catch (error) {
+                    console.error(`Error deserializing item with id ${id}:`, error);
+                }
+            }
         }
         return deserializedItems;
     }
 
     replaceReferences(item) {
         const replaceReference = (element) => {
+            console.log(element);
             if (element && element.id && element.key && element.reference) {
                 const itemClass = LocalStorage.savable_classes[element.key];
                 return this.getItemById(element.id, itemClass) || null;
@@ -55,27 +64,16 @@ export class LocalStorage {
             return element;
         };
 
-        const handleReference = (key) => {
-            if (Array.isArray(item[key])) {
-                item[key] = item[key].map(replaceReference).filter(element => {
-                    if (element === null) {
-                        console.error(`Reference not found for ${item.constructor.name} ${item.id}`);
-                        return false;
-                    }
-                    return true;
-                });
-            } else {
-                item[key] = replaceReference(item[key]);
-                if (item[key] === null) {
-                    delete item[key];
-                    console.error(`Reference not found for ${item.constructor.name} ${item.id}`);
-                }
-            }
-        };
-
         for (const key in item) {
             if (item.hasOwnProperty(key)) {
-                handleReference(key);
+                if (Array.isArray(item[key])) {
+                    item[key] = item[key].map(replaceReference).filter(Boolean);
+                } else {
+                    item[key] = replaceReference(item[key]);
+                    if (item[key] === null) {
+                        delete item[key];
+                    }
+                }
             }
         }
         return item;
@@ -83,24 +81,24 @@ export class LocalStorage {
 
     replaceToReference(item) {
         for (const key in item) {
-            if (item[key] instanceof Savable) {
-                item[key] = item[key].toReference();
-            } else if (Array.isArray(item[key])) {
-
-                item[key].forEach((element, index) => {
-                    if (element instanceof Savable) {
-                        this.addItem(element);
-                        item[key][index] = element.toReference();
-                    }
-                });
+            if (item.hasOwnProperty(key)) {
+                if (item[key] instanceof Savable) {
+                    item[key] = item[key].toReference();
+                } else if (Array.isArray(item[key])) {
+                    item[key] = item[key].map(element => {
+                        if (element instanceof Savable) {
+                            this.addItem(element);
+                            return element.toReference();
+                        }
+                        return element;
+                    });
+                }
             }
         }
-
         return item;
     }
 
     getItemById(id, itemClass) {
-        const key = `${itemClass.name.toLowerCase()}_storage`;
         const items = this.getAllItems(itemClass);
         return items[id] || null;
     }
